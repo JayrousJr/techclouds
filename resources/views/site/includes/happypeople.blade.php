45659dc8c45e0a86484a3bf2ecf9a982585105fb
Joshua Jayrous <joshuajayrous@users.noreply.github.com>
2023-12-14 23:30:18 +0300

    Update happypeople.blade.php

--- resources/views/site/includes/happypeople.blade.php
+++ resources/views/site/includes/happypeople.blade.php
@@ -1,49 +1,49 @@
 <section class="site-section">
-  <div class="container">
-    <div class="row mb-5">
-      <div class="col-12 text-center" data-aos="fade">
-        <h4 class="section-title mb-3">What happy People say</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="block__87154">
-          <span class="quote"><img src="/storage/images/data/quote.png" alt="Image"></span>
-          <blockquote>
-            <p>I think this is the idea that we have come up with, and we all have the same focus to reach
-              the technology that we have never think of reaching. We need a little push to get there</p>
-          </blockquote>
-          <div class="block__91147 d-flex align-items-center">
-            <figure class="mr-4"><img src="/storage/images/data/jayrous.jpg" alt="Image" class="img-fluid">
-            </figure>
-            <div>
-              <h3>Jayrous Joshua</h3>
-              <span class="position">Creative Director</span>
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-12 text-center" data-aos="fade">
+                <h4 class="section-title mb-3">What happy People say</h4>
             </div>
-          </div>
         </div>
-      </div>
-
-      <div class="col-lg-6">
-        <div class="block__87154">
-          <span class="quote"><img src="/storage/images/data/quote.png" alt="Image"></span>
-          <blockquote>
-            <p>For all my years of studying, i have only wanted to make things into practice, out of having
-              good life with my family, i have been thoughtfully to make my knowledge and talent in use,
-              thanks to Techclouds Idea</p>
-          </blockquote>
-          <div class="block__91147 d-flex align-items-center">
-            <figure class="mr-4"><img src="/storage/images/data/joyder.jpg" alt="Image" class="img-fluid">
-            </figure>
-            <div>
-              <h3>Edrick Kartabarula</h3>
-              <span class="position">Web Designer</span>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="block__87154">
+                    <span class="quote"><img src="/storage/images/data/quote.png" alt="Image"></span>
+                    <blockquote>
+                        <p>I think this is the idea that we have come up with, and we all have the same focus to reach
+                            the technology that we have never think of reaching. We need a little push to get there</p>
+                    </blockquote>
+                    <div class="block__91147 d-flex align-items-center">
+                        <figure class="mr-4"><img src="/storage/images/data/jayrous.jpg" alt="Image" class="img-fluid">
+                        </figure>
+                        <div>
+                            <h3>Jayrous Joshua</h3>
+                            <span class="position">Creative Director</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <div class="col-lg-6">
+                <div class="block__87154">
+                    <span class="quote"><img src="/storage/images/data/quote.png" alt="Image"></span>
+                    <blockquote>
+                        <p>For all my years of studying, i have only wanted to make things into practice, out of having
+                            good life with my family, i have been thoughtfully to make my knowledge and talent in use,
+                            thanks to Techclouds Idea</p>
+                    </blockquote>
+                    <div class="block__91147 d-flex align-items-center">
+                        <figure class="mr-4"><img src="/storage/images/data/joyder.jpg" alt="Image" class="img-fluid">
+                        </figure>
+                        <div>
+                            <h3>Joyder Jovin</h3>
+                            <span class="position">Python Lead</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
-      </div>
-
-
     </div>
-  </div>
 </section>
