bf3521d5fda0f050a764002bc80c0af79fbbd06d
Joshua Jayrous <111858219+JayrousJr@users.noreply.github.com>
2023-12-22 12:15:49 +0300

    double commits

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -46,7 +46,7 @@
         <div class="row text-center">
             <div class="col-md-6 col-s-12 ">
                 <p class="copyright">Copyright &copy;<script>
-                    document.write(new Date().getFullYear());
+                        document.write(new Date().getFullYear());
                     </script>
                 </p>
             </div>
@@ -101,19 +101,19 @@
 <script src="https://chatbolt.ai/widget/addee1f1-f0af-44f6-b0a8-fd721c0acdea.js"></script>
 
 <script>
-$(document).ready(function() {
-    //hide the notification after 2seconds  
-    setTimeout(function() {
-        $("#notification").fadeOut('slow');
-    }, 15000);
-});
+    $(document).ready(function() {
+        //hide the notification after 2seconds  
+        setTimeout(function() {
+            $("#notification").fadeOut('slow');
+        }, 15000);
+    });
 
-$(document).ready(function() {
-    //hide the notification after 2seconds  
-    setTimeout(function() {
-        $("#feddback").fadeOut('slow');
-    }, 5000);
-});
+    $(document).ready(function() {
+        //hide the notification after 2seconds  
+        setTimeout(function() {
+            $("#feddback").fadeOut('slow');
+        }, 5000);
+    });
 </script>
 <!--chat bot-->
 <!-- <script defer async>
@@ -143,19 +143,19 @@ document.addEventListener('DOMContentLoaded', function() {
 </script> -->
 <!--chat bot-->
 <script>
-$(document).ready(function() {
-    //hide the notification after 2seconds  
-    setTimeout(function() {
-        $("#notification").fadeOut('slow');
-    }, 15000);
-});
+    $(document).ready(function() {
+        //hide the notification after 2seconds  
+        setTimeout(function() {
+            $("#notification").fadeOut('slow');
+        }, 15000);
+    });
 
-$(document).ready(function() {
-    //hide the notification after 2seconds  
-    setTimeout(function() {
-        $("#feddback").fadeOut('slow');
-    }, 5000);
-});
+    $(document).ready(function() {
+        //hide the notification after 2seconds  
+        setTimeout(function() {
+            $("#feddback").fadeOut('slow');
+        }, 5000);
+    });
 </script>
 </body>
 
